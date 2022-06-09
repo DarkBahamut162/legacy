818a0b0edf31f39d84027bf9f04b4a3579443481
DarkBahamut162 <dark_bahamut162@hotmail.com>
2022-06-09 20:38:48 +0200

    Display SortOrderFrame only in NonCourseModes

--- Graphics/ScreenSelectMusic SortOrderFrame.lua
+++ Graphics/ScreenSelectMusic SortOrderFrame.lua
@@ -1,8 +1,10 @@
+local checkVisibility = not (GAMESTATE:GetPlayMode() == "PlayMode_Endless" or GAMESTATE:GetPlayMode() == "PlayMode_Nonstop" or GAMESTATE:GetPlayMode() == "PlayMode_Oni")
+
 return Def.ActorFrame {
 	LoadActor(THEME:GetPathG("OptionRowExit","frame")) .. {
-		InitCommand=function(self) self:diffusebottomedge(Color("Orange")) end;
+		InitCommand=function(self) self:diffusebottomedge(Color("Orange")):visible(checkVisibility); end;
 	};
 	LoadActor(THEME:GetPathG("_icon","Sort")) .. {
-		InitCommand=function(self) self:x(-60):shadowlength(1):diffuse(Color("Orange")):diffusetopedge(Color("Yellow")) end;
+		InitCommand=function(self) self:x(-60):shadowlength(1):diffuse(Color("Orange")):diffusetopedge(Color("Yellow")):visible(checkVisibility); end;
 	};
 };
