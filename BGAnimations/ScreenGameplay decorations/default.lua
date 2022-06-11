33394eb1fc192c7d79daa7fbe27422b8f95e80b9
DarkBahamut162 <dark_bahamut162@hotmail.com>
2022-06-11 09:57:07 +0200

    Readd 2P-Segments in ScreenGameplay because of SSC

--- BGAnimations/ScreenGameplay decorations/default.lua
+++ BGAnimations/ScreenGameplay decorations/default.lua
@@ -17,7 +17,7 @@ local function CreateSegments(Player)
 	if not GAMESTATE:IsCourseMode() then
 	-- Straight rip off NCRX
 		local song = GAMESTATE:GetCurrentSong();
-		local step = GAMESTATE:GetCurrentSteps(GAMESTATE:GetMasterPlayerNumber());
+		local step = GAMESTATE:GetCurrentSteps(Player);
 		local timingData = song:GetTimingData();
 		-- use the StepsSeconds, which will almost always be more proper
 		-- than a file with ITG2r21 compatibility.
@@ -29,100 +29,100 @@ local function CreateSegments(Player)
 
 			if step then
 				timingData = step:GetTimingData();
+
+				local bpms = timingData:GetBPMsAndTimes(true);
+				local stops = timingData:GetStops(true);
+				local delays = timingData:GetDelays(true);
+				local warps = timingData:GetWarps(true);
+				local fakes = timingData:GetFakes(true);
+				local scrolls = timingData:GetScrolls(true);
+				local speeds = timingData:GetSpeeds(true);
+
+				local function CreateLine(beat, secs, firstShadow, firstDiffuse, secondShadow, firstEffect, secondEffect)
+					local beatTime = timingData:GetElapsedTimeFromBeat(beat);
+					if beatTime < 0 then beatTime = 0; end;
+					return Def.ActorFrame {
+						Def.Quad {
+							InitCommand=function(self)
+								self:shadowlength(0);
+								self:shadowcolor(color(firstShadow));
+								-- set width
+								self:zoomto(math.max((secs/songLen)*fFrameWidth, 1), fFrameHeight);
+								-- find location
+								self:x((scale(beatTime,firstBeatSecs,lastBeatSecs,-fFrameWidth/2,fFrameWidth/2)));
+							end;
+							OnCommand=function(self)
+								self:diffuse(color(firstDiffuse));
+								self:sleep(beatTime+1);
+								self:linear(2);
+								self:diffusealpha(0);
+							end;
+						};
+						-- there's a cool effect that can't happen because we don't fade out like we did before
+						Def.Quad {
+							InitCommand=function(self)
+								--self:diffuse(HSVA(192,1,0.8,0.8));
+								self:shadowlength(0);
+								self:shadowcolor(color(secondShadow));
+								-- set width
+								self:zoomto(math.max((secs/songLen)*fFrameWidth, 1),fFrameHeight);
+								-- find location
+								self:x((scale(beatTime,firstBeatSecs,lastBeatSecs,-fFrameWidth/2,fFrameWidth/2)));
+							end;
+							OnCommand=function(self)
+								self:diffusealpha(1);
+								self:diffuseshift();
+								self:effectcolor1(color(firstEffect));
+								self:effectcolor2(color(secondEffect));
+								self:effectclock('beat');
+								self:effectperiod(1/8);
+								--
+								self:diffusealpha(0);
+								self:sleep(beatTime+1);
+								self:diffusealpha(1);
+								self:linear(4);
+								self:diffusealpha(0);
+							end;
+						};
+					};
+				end;
+
+				for i=2,#bpms do
+					bpmFrame[#bpmFrame+1] = CreateLine(bpms[i][1], 0,
+						"#00808077", "#00808077", "#00808077", "#FF634777", "#FF000077");
+				end;
+
+				for i=1,#delays do
+					delayFrame[#delayFrame+1] = CreateLine(delays[i][1], delays[i][2],
+						"#FFFF0077", "#FFFF0077", "#FFFF0077", "#00FF0077", "#FF000077");
+				end;
+
+				for i=1,#stops do
+					stopFrame[#stopFrame+1] = CreateLine(stops[i][1], stops[i][2],
+						"#FFFFFF77", "#FFFFFF77", "#FFFFFF77", "#FFA50077", "#FF000077");
+				end;
+
+				for i=1,#scrolls do
+					scrollFrame[#scrollFrame+1] = CreateLine(scrolls[i][1], 0,
+						"#4169E177", "#4169E177", "#4169E177", "#0000FF77", "#FF000077");
+				end;
+
+				for i=1,#speeds do
+					-- TODO: Turn beats into seconds for this calculation?
+					speedFrame[#speedFrame+1] = CreateLine(speeds[i][1], 0,
+						"#ADFF2F77", "#ADFF2F77", "#ADFF2F77", "#7CFC0077", "#FF000077");
+				end;
+
+				for i=1,#warps do
+					warpFrame[#warpFrame+1] = CreateLine(warps[i][1], 0,
+						"#CC00CC77", "#CC00CC77", "#CC00CC77", "#FF33CC77", "#FF000077");
+				end;
+
+				for i=1,#fakes do
+					fakeFrame[#fakeFrame+1] = CreateLine(fakes[i][1], 0,
+						"#BC8F8F77", "#BC8F8F77", "#BC8F8F77", "#F4A46077", "#FF000077");
+				end;
 			end
-
-			local bpms = timingData:GetBPMsAndTimes(true);
-			local stops = timingData:GetStops(true);
-			local delays = timingData:GetDelays(true);
-			local warps = timingData:GetWarps(true);
-			local fakes = timingData:GetFakes(true);
-			local scrolls = timingData:GetScrolls(true);
-			local speeds = timingData:GetSpeeds(true);
-
-			local function CreateLine(beat, secs, firstShadow, firstDiffuse, secondShadow, firstEffect, secondEffect)
-				local beatTime = timingData:GetElapsedTimeFromBeat(beat);
-				if beatTime < 0 then beatTime = 0; end;
-				return Def.ActorFrame {
-					Def.Quad {
-						InitCommand=function(self)
-							self:shadowlength(0);
-							self:shadowcolor(color(firstShadow));
-							-- set width
-							self:zoomto(math.max((secs/songLen)*fFrameWidth, 1), fFrameHeight);
-							-- find location
-							self:x((scale(beatTime,firstBeatSecs,lastBeatSecs,-fFrameWidth/2,fFrameWidth/2)));
-						end;
-						OnCommand=function(self)
-							self:diffuse(color(firstDiffuse));
-							self:sleep(beatTime+1);
-							self:linear(2);
-							self:diffusealpha(0);
-						end;
-					};
-					-- there's a cool effect that can't happen because we don't fade out like we did before
-					Def.Quad {
-						InitCommand=function(self)
-							--self:diffuse(HSVA(192,1,0.8,0.8));
-							self:shadowlength(0);
-							self:shadowcolor(color(secondShadow));
-							-- set width
-							self:zoomto(math.max((secs/songLen)*fFrameWidth, 1),fFrameHeight);
-							-- find location
-							self:x((scale(beatTime,firstBeatSecs,lastBeatSecs,-fFrameWidth/2,fFrameWidth/2)));
-						end;
-						OnCommand=function(self)
-							self:diffusealpha(1);
-							self:diffuseshift();
-							self:effectcolor1(color(firstEffect));
-							self:effectcolor2(color(secondEffect));
-							self:effectclock('beat');
-							self:effectperiod(1/8);
-							--
-							self:diffusealpha(0);
-							self:sleep(beatTime+1);
-							self:diffusealpha(1);
-							self:linear(4);
-							self:diffusealpha(0);
-						end;
-					};
-				};
-			end;
-
-			for i=2,#bpms do
-				bpmFrame[#bpmFrame+1] = CreateLine(bpms[i][1], 0,
-					"#00808077", "#00808077", "#00808077", "#FF634777", "#FF000077");
-			end;
-
-			for i=1,#delays do
-				delayFrame[#delayFrame+1] = CreateLine(delays[i][1], delays[i][2],
-					"#FFFF0077", "#FFFF0077", "#FFFF0077", "#00FF0077", "#FF000077");
-			end;
-
-			for i=1,#stops do
-				stopFrame[#stopFrame+1] = CreateLine(stops[i][1], stops[i][2],
-					"#FFFFFF77", "#FFFFFF77", "#FFFFFF77", "#FFA50077", "#FF000077");
-			end;
-
-			for i=1,#scrolls do
-				scrollFrame[#scrollFrame+1] = CreateLine(scrolls[i][1], 0,
-					"#4169E177", "#4169E177", "#4169E177", "#0000FF77", "#FF000077");
-			end;
-
-			for i=1,#speeds do
-				-- TODO: Turn beats into seconds for this calculation?
-				speedFrame[#speedFrame+1] = CreateLine(speeds[i][1], 0,
-					"#ADFF2F77", "#ADFF2F77", "#ADFF2F77", "#7CFC0077", "#FF000077");
-			end;
-
-			for i=1,#warps do
-				warpFrame[#warpFrame+1] = CreateLine(warps[i][1], 0,
-					"#CC00CC77", "#CC00CC77", "#CC00CC77", "#FF33CC77", "#FF000077");
-			end;
-
-			for i=1,#fakes do
-				fakeFrame[#fakeFrame+1] = CreateLine(fakes[i][1], 0,
-					"#BC8F8F77", "#BC8F8F77", "#BC8F8F77", "#F4A46077", "#FF000077");
-			end;
 		end;
 		bars[#bars+1] = bpmFrame;
 		bars[#bars+1] = scrollFrame;
@@ -167,14 +167,19 @@ t[#t+1] = StandardDecorationFromFileOptional("ScoreFrame","ScoreFrame");
 
 local function songMeterScale(val) return scale(val,0,1,-380/2,380/2) end
 
---for pn in ivalues(PlayerNumber) do
-	local MetricsName = "SongMeterDisplay";
+for pn in ivalues(PlayerNumber) do
+	local MetricsName;
+	if GAMESTATE:GetNumPlayersEnabled()==1 then
+		MetricsName = "SongMeterDisplay"
+	else
+		MetricsName = "SongMeterDisplay" .. PlayerNumberToString(pn)
+	end
 	local songMeterDisplay = Def.ActorFrame{
 		InitCommand=function(self)
 			self:name(MetricsName); 
 			ActorUtil.LoadAllCommandsAndSetXY(self,Var "LoadingScreen"); 
 		end;
- 		LoadActor( THEME:GetPathG( 'SongMeterDisplay', 'frame ' .. PlayerNumberToString(PLAYER_1) ) ) .. {
+		LoadActor( THEME:GetPathG( 'SongMeterDisplay', 'frame ' .. PlayerNumberToString(pn) ) ) .. {
 			InitCommand=function(self)
 				self:name('Frame'); 
 				ActorUtil.LoadAllCommandsAndSetXY(self,MetricsName); 
@@ -194,17 +199,17 @@ local function songMeterScale(val) return scale(val,0,1,-380/2,380/2) end
 		};
 		Def.SongMeterDisplay {
 			StreamWidth=THEME:GetMetric( MetricsName, 'StreamWidth' );
-			Stream=LoadActor( THEME:GetPathG( 'SongMeterDisplay', 'stream ' .. PlayerNumberToString(PLAYER_1) ) )..{
+			Stream=LoadActor( THEME:GetPathG( 'SongMeterDisplay', 'stream ' .. PlayerNumberToString(pn) ) )..{
 				InitCommand=function(self) self:diffuse(Color("Orange")):diffusealpha(0.5):blend(Blend.Add) end;
 			};
-			Tip=LoadActor( THEME:GetPathG( 'SongMeterDisplay', 'tip ' .. PlayerNumberToString(PLAYER_1) ) ) .. { InitCommand=function(self) self:visible(false) end; };
+			Tip=LoadActor( THEME:GetPathG( 'SongMeterDisplay', 'tip ' .. PlayerNumberToString(pn) ) ) .. { InitCommand=function(self) self:visible(false) end; };
 		};
 	};
 	--if ThemePrefs.Get("TimingDisplay") == true then
-		songMeterDisplay[#songMeterDisplay+1] = CreateSegments();
+		songMeterDisplay[#songMeterDisplay+1] = CreateSegments(pn);
 	--end
 	t[#t+1] = songMeterDisplay
---end;
+end;
 
 for pn in ivalues(PlayerNumber) do
 	local MetricsName = "ToastyDisplay" .. PlayerNumberToString(pn);
