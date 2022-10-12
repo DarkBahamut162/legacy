e7bbe72271af5210f29d949fa00aad27ef634b39
DarkBahamut162 <dark_bahamut162@hotmail.com>
2022-10-12 09:20:43 +0200

    Fixing something

--- BGAnimations/ScreenWithMenuElements background/default.lua
+++ BGAnimations/ScreenWithMenuElements background/default.lua
@@ -7,18 +7,16 @@ return Def.ActorFrame {
 			InitCommand=function(self) self:scaletoclipped(SCREEN_WIDTH,SCREEN_HEIGHT) end,
 			OnCommand=function(self) self:diffuse(color("#FFCB05")):diffusebottomedge(color("#F0BA00")) end
 		},
-	},
-	Def.ActorFrame {
-		LoadActor("_checkerboard") .. {
-			InitCommand=function(self)
-				self:rotationx(-90/4*3.5):zoomto(SCREEN_WIDTH*2,SCREEN_HEIGHT*2):customtexturerect(0,0,SCREEN_WIDTH*4/256,SCREEN_HEIGHT*4/256)
-			end,
-			OnCommand=function(self) self:texcoordvelocity(0,0.25):diffuse(color("#ffd400")):fadetop(1) end
+		Def.ActorFrame {
+			LoadActor("_checkerboard")..{
+				InitCommand=function(self)
+					self:rotationx(-90/4*3.5):zoomto(SCREEN_WIDTH*2,SCREEN_HEIGHT*2):customtexturerect(0,0,SCREEN_WIDTH*4/256,SCREEN_HEIGHT*4/256)
+				end,
+				OnCommand=function(self) self:texcoordvelocity(0,0.25):diffuse(color("#ffd400")):fadetop(1) end
+			}
+		},
+		LoadActor("_particleLoader")..{
+			InitCommand=function(self) self:xy(-SCREEN_CENTER_X,-SCREEN_CENTER_Y) end
 		}
-	},
-	LoadActor("_particleLoader") .. {
-		InitCommand=function(self)
-			self:xy(-SCREEN_CENTER_X,-SCREEN_CENTER_Y)
-		end
 	}
 }
