af222aebfea7c4c364706ee464f7d1a6abac222f
DarkBahamut162 <dark_bahamut162@hotmail.com>
2024-01-29 11:06:28 +0100

    Fix Rave being both centered...

--- BGAnimations/ScreenGameplay background.lua
+++ BGAnimations/ScreenGameplay background.lua
@@ -1,1 +1,12 @@
-return Def.ActorFrame { Name="YOU_WISH_YOU_WERE_PLAYING_BEATMANIA_RIGHT_NOW" }
+return Def.ActorFrame {
+    Name="YOU_WISH_YOU_WERE_PLAYING_BEATMANIA_RIGHT_NOW",
+    OnCommand=function(self)
+        for pn in ivalues(GAMESTATE:GetEnabledPlayers()) do
+            if GAMESTATE:GetPlayMode() == 'PlayMode_Rave' then
+                if SCREENMAN:GetTopScreen():GetChild("Player"..pname(pn)) then
+					SCREENMAN:GetTopScreen():GetChild("Player"..pname(pn)):x(THEME:GetMetric("ScreenGameplay","Player"..pname(pn).."OnePlayerOneSideX"))
+                end
+            end
+        end
+    end
+}
